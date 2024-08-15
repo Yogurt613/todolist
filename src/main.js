cf2f0a6fae894f0ba2afa4322f3680121cc05a28
Stella <hanhanyang399@gmail.com>
2024-08-15 11:30:44 +0800

    Add bootstrap via npm instead of cdn

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { Popover } from 'bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './style.scss';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    new Popover(popover);
+  });
